c0566b23e80205c39dec55e4e5222870a90d39f1
Shahin Hemmati <32401073+shahinam2@users.noreply.github.com>
2022-04-23 12:11:22 +0200

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # the following script assumes that you are using a clean install of Ubuntu.
 
-# setups the repo:
+# Preprations:
 sudo apt-get update &> /dev/null
 echo -e "\n======================================="
 echo "Updating apt & setting up the environment "
@@ -16,6 +16,10 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list &> /dev/null
 
+###########################################################
+####################Installation Part######################
+###########################################################
+
 # Install Docker Engine
 sudo apt-get update &> /dev/null
 echo -e "\n======================================="
@@ -27,30 +31,15 @@ echo "======================================="  >> docker-install.log
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y &>> docker-install.log
 sleep 2
 
-# verify the presence of docker packages
-echo -e "\n=======================================" >> docker-install.log
-echo "Verify the presence of docker packages:" >> docker-install.log
-echo "=======================================" >> docker-install.log
-sudo dpkg -l | grep -i docker >> docker-install.log
-
-# verify the installed software version
-echo -e "\n=======================================" >> docker-install.log
-echo "Verify docker version" >> docker-install.log
-echo "=======================================" >> docker-install.log
-sudo docker --version >> docker-install.log
-
-# get the details of server/client model:
-echo -e "\n=======================================" >> docker-install.log
-echo "Verify the server/client model infos" >> docker-install.log
-echo "=======================================" >> docker-install.log
-sudo docker version >> docker-install.log
-
-# to show the information about the docker host:
-# number of running containers, number of images, default registry, etc...
-echo -e "\n=======================================" >> docker-install.log
-echo "Verify the docker host infos" >> docker-install.log
-echo "=======================================" >> docker-install.log
-sudo docker system info >> docker-install.log
+# installing docker compose version 2:
+echo -e "\n======================================="
+echo "Installing Docker Compose V2"
+echo "======================================="
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose &>/dev/null
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+sleep 2
 
 # To manage docker as non-root user
 echo -e "\n======================================="
@@ -66,6 +55,42 @@ echo "Installation logs are stored in ./docker-install.log"
 echo "======================================="
 sleep 2
 
+#############################################################
+#################Verification Part###########################
+#############################################################
+# verify the presence of docker packages
+echo -e "\n=======================================" >> docker-install.log
+echo "Verify the presence of docker packages:" >> docker-install.log
+echo "=======================================" >> docker-install.log
+sudo dpkg -l | grep -i docker >> docker-install.log
+
+# verify the installed software version
+echo -e "\n=======================================" >> docker-install.log
+echo "Verify docker version" >> docker-install.log
+echo "=======================================" >> docker-install.log
+docker --version >> docker-install.log
+
+# get the details of server/client model:
+echo -e "\n=======================================" >> docker-install.log
+echo "Verify the server/client model infos" >> docker-install.log
+echo "=======================================" >> docker-install.log
+docker version >> docker-install.log
+
+# to show the information about the docker host:
+# number of running containers, number of images, default registry, etc...
+echo -e "\n=======================================" >> docker-install.log
+echo "Verify the docker host infos" >> docker-install.log
+echo "=======================================" >> docker-install.log
+docker system info >> docker-install.log
+
+echo -e "\n=======================================" >> docker-install.log
+echo "Verify the docker compose version" >> docker-install.log
+echo "=======================================" >> docker-install.log
+docker compose version >> docker-install.log
+
+####################################################
+###################System Reboot####################
+####################################################
 # reboot the system
 echo -e "\n======================================="
 echo "Rebooting system in 5 seconds"
