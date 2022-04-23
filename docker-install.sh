b43298cba6bbad30c12d7288cce8f94da62caf69
Shahin Hemmati <32401073+shahinam2@users.noreply.github.com>
2022-04-23 13:33:02 +0200

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -35,7 +35,7 @@ sleep 2
 echo -e "\n======================================="
 echo "Installing Docker Compose V2"
 echo "======================================="
-DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+DOCKER_CONFIG=${DOCKER_CONFIG:-/usr/local/lib/docker/cli-plugins}
 mkdir -p $DOCKER_CONFIG/cli-plugins
 curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose &>/dev/null
 chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
